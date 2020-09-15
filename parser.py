a7d01c1328525efa43c6727439e38f15ffbe41be
Iglek Kuat <dizinnes228@gmail.com>
2020-09-15 18:29:16 +0600

    fit: add user-agent and url

--- parser.py
+++ parser.py
@@ -2,3 +2,13 @@ from aiohttp import ClientSession
 from asyncio import get_event_loop
 
 from bs4 import BeautifulSoup
+
+
+user_agent = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+}
+url = 'https://www.olx.kz/kokshetau/q-%D0%BA%D1%80%D0%BE%D0%BB%D0%B8%D0%BA/'
+
+
+
+
