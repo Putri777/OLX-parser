28a8c4c1ca86051c23e94d47c499b18cf7e6c7d2
Iglek Kuat <dizinnes228@gmail.com>
2020-09-15 18:32:13 +0600

    fit: add request function

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from aiohttp import ClientSession
 from asyncio import get_event_loop
 
@@ -7,7 +9,14 @@ from bs4 import BeautifulSoup
 user_agent = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
 }
-url = 'https://www.olx.kz/kokshetau/q-%D0%BA%D1%80%D0%BE%D0%BB%D0%B8%D0%BA/'
+olx_link = 'https://www.olx.kz/kokshetau/q-%D0%BA%D1%80%D0%BE%D0%BB%D0%B8%D0%BA/'
+
+
+async def request(headers: Dict, url: str) -> None:
+    session = ClientSession()
+    async with session.get(headers=headers, url=url) as response:
+        content = await response.text()
+        print(content)
 
 
 
