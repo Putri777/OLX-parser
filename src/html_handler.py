ee1a685d33de99d7c4346a4508629d3bc1e48627
Iglek Kuat <dizinnes228@gmail.com>
2020-09-16 21:19:54 +0600

    refactor: rewrite code to OOP,

--- src/html_handler.py
+++ src/html_handler.py
@@ -1,27 +1,33 @@
 from asyncio import get_event_loop
+from typing import Dict
 
 from bs4 import BeautifulSoup
 
-from .request import request
+from .request import Request
 
 
-async def html_processing() -> None:
-    product = str(input('Введите название продукта: '))
-    soup = BeautifulSoup(await request(product), 'lxml')
-    for html in soup.select('div.offer-wrapper'):
-        title = html.select_one('a.marginright5.link.linkWithHash.detailsLink strong').get_text()
-        price = html.select_one('p.price>strong').get_text()
-        data = html.select_one('small.breadcrumb.x-normal>span').get_text()
-        url = html.select_one('a.marginright5.link.linkWithHash.detailsLink')['href']
+class HtmlHandler:
+    def __init__(self, request: Request):
+        self.request = request
 
-        print(f'''
-Название товара: {title},
-Цена товара: {price},
-Дата публикации: {data},
-Ссылка на товар: {url}
-        ''')
+    async def html_processing(self) -> None:
+        # product = str(input('Введите название продукта: '))
+        soup = BeautifulSoup(await self.request.request(), 'lxml')
+        for html in soup.select('div.offer-wrapper'):
+            title = html.select_one('a.marginright5.link.linkWithHash.detailsLink strong').get_text()
+            price = html.select_one('p.price>strong').get_text()
+            data = html.select_one('small.breadcrumb.x-normal>span').get_text()
+            url = html.select_one('a.marginright5.link.linkWithHash.detailsLink')['href']
+
+            print(f'''
+    Название товара: {title},
+    Цена товара: {price},
+    Дата публикации: {data},
+    Ссылка на товар: {url}
+            ''')
 
 
-if __name__ == '__main__':
-    loop = get_event_loop()
-    loop.run_until_complete(html_processing())
+# if __name__ == '__main__':
+#     html_handler = HtmlHandler(Request({'User-Agent'}, 'qwe'))
+#     loop = get_event_loop()
+#     loop.run_until_complete(html_handler.html_processing())
