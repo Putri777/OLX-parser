3cc9988050fcd1328a84a718678060ad5fe55dc2
Iglek Kuat <dizinnes228@gmail.com>
2020-09-16 21:19:58 +0600

    refactor: rewrite code to OOP,

--- src/request.py
+++ src/request.py
@@ -1,15 +1,21 @@
+from typing import Dict
+
 from aiohttp import ClientSession
 
-user_agent = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
-}
+# user_agent = {
+#     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+# }
 
 
-async def request(product: str) -> str:
-    session = ClientSession()
-    async with session.get(headers=user_agent, url=f'https://www.olx.kz/kokshetau/q-{product}') as response:
-        content = await response.text()
-    await session.close()
+class Request:
+    def __init__(self, user_agent: Dict, product: str) -> None:
+        self.user_agent = user_agent
+        self.product = product
 
-    return content
+    async def request(self) -> str:
+        session = ClientSession()
+        async with session.get(headers=self.user_agent, url=f'https://www.olx.kz/kokshetau/q-{self.product}') as response:
+            content = await response.text()
+        await session.close()
 
+        return content
