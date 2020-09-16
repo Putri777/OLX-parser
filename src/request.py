7164d5a3c62f3c99c70be4c8042a71a0589ba184
Iglek Kuat <dizinnes228@gmail.com>
2020-09-16 17:07:55 +0600

    fit: add option for fucntion

--- src/request.py
+++ src/request.py
@@ -1,17 +1,11 @@
-from typing import Dict
-
 from aiohttp import ClientSession
-from asyncio import get_event_loop
-
-from bs4 import BeautifulSoup
 
 user_agent = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
 }
 
 
-async def request() -> str:
-    product = str(input('Введите название продукта: '))
+async def request(product: str) -> str:
     session = ClientSession()
     async with session.get(headers=user_agent, url=f'https://www.olx.kz/kokshetau/q-{product}') as response:
         content = await response.text()
@@ -19,8 +13,3 @@ async def request() -> str:
 
     return content
 
-
-if __name__ == '__main__':
-    loop = get_event_loop()
-    # loop.run_until_complete(html_processing())
-    print(loop.run_until_complete(request()))
