e2a40ca5c6d8d3b862fd241f31ec5ee739c3e0d5
Iglek Kuat <dizinnes228@gmail.com>
2020-09-16 16:46:04 +0600

    fit: update function

--- src/selenium_parser.py
+++ src/selenium_parser.py
@@ -1,13 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('/home/dizinnes/Downloads/chromedriver')
-# driver.get("https://www.olx.kz/kokshetau/")
-driver.get("http://www.python.org")
+import time
 
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+
+def selenium_html() -> None:
+    product = str(input('Введите товар который хотите искать: '))
+    driver = webdriver.Chrome('/home/dizinnes/Downloads/chromedriver')
+    driver.get("https://www.olx.kz/kokshetau/q-" + product + '?page=2')
+    # elem = driver.find_element_by_name("q")
+    # elem.send_keys(product)
+    # elem.send_keys(Keys.RETURN)
+    # elem.click()
+    assert "No results found." not in driver.page_source
+    print(driver.page_source)
+    time.sleep(5)
+    driver.close()
+
+
+selenium_html()
