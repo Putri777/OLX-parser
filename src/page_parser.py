17b9105d64dd4543bf4c63cffe756739d47fb340
Iglek Kuat <dizinnes228@gmail.com>
2020-09-17 19:02:34 +0600

    fit: add list return

--- src/page_parser.py
+++ src/page_parser.py
@@ -9,12 +9,15 @@ class PageParser:
     def __init__(self, request: Request):
         self.request = request
 
-    async def page_parse(self):
+    async def page_parse(self) -> List:
+        # product = str(input('Ведите название продукта: '))
+        pages_list = []
         soup = BeautifulSoup(await self.request.request('квартиры'), 'lxml')
         for pages in soup.select('div.pager.rel.clr>span.item.fleft'):
             try:
-                b = pages.select_one('a')['href'] if pages.select_one('a')['href'] is not None else None
-                print(b)
+                pages_list.append(pages.select_one('a')['href'] if pages.select_one('a')['href'] is not None else None)
             except TypeError as ex:
                 # 'NoneType' object is not subscriptable
                 pass
+
+        return pages_list
