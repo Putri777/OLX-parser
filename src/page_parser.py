c90b66df139a9f1b76d9db540e8076730ee0a3f6
Iglek Kuat <dizinnes228@gmail.com>
2020-09-17 18:15:51 +0600

    fit: add class and methods

--- src/page_parser.py
+++ src/page_parser.py
@@ -3,3 +3,11 @@ from typing import List
 from bs4 import BeautifulSoup
 
 from .request import Request
+
+
+class PageParser:
+    def __init__(self):
+        pass
+
+    async def page_parse(self):
+        pass
