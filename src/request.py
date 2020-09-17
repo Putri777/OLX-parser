e60ba5d5b029fb5df496fad492a3703eececb7c1
Iglek Kuat <dizinnes228@gmail.com>
2020-09-17 20:02:31 +0600

    docs: add doc to constructor and method for request.py

--- src/request.py
+++ src/request.py
@@ -5,9 +5,18 @@ from aiohttp import ClientSession
 
 class Request:
     def __init__(self, user_agent: Dict) -> None:
+        """
+        Constructor takes one parameter `user_agent`
+        :param user_agent: Dict
+        """
         self.user_agent = user_agent
 
     async def request(self, product: str) -> str:
+        """
+        The function make request
+        :param product: str
+        :return: str
+        """
         session = ClientSession()
         async with session.get(headers=self.user_agent,
                                url=f'https://www.olx.kz/kokshetau/q-{product}') as response:
