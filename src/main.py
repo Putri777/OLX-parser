23f655797818117b7eff9df2d2ef89e24851116f
Iglek Kuat <dizinnes228@gmail.com>
2020-09-17 19:56:53 +0600

    docs: add doc to constructor and method for main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,11 @@ from .page_parser import PageParser
 from .request import Request
 
 
-async def async_main():
+async def async_main() -> None:
+    """
+    When executing the function, it takes the entire method from src and sends it to `runner.py`
+    :return: None
+    """
     user_agent = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
     }
@@ -17,5 +21,9 @@ async def async_main():
     print(await page_parser.page_parse())
 
 
-def main():
+def main() -> None:
+    """
+    Get async function from async_main() and run here
+    :return:
+    """
     run(async_main())
