1f7dcb4cd466f646474cb860c89cfff042cded23
Iglek Kuat <dizinnes228@gmail.com>
2020-09-17 18:17:33 +0600

    fit: add page_parse method and add request in constructor

--- src/page_parser.py
+++ src/page_parser.py
@@ -6,8 +6,15 @@ from .request import Request
 
 
 class PageParser:
-    def __init__(self):
-        pass
+    def __init__(self, request: Request):
+        self.request = request
 
     async def page_parse(self):
-        pass
+        soup = BeautifulSoup(await self.request.request('кролик'), 'lxml')
+        for pages in soup.select('div.pager.rel.clr>span.item.fleft'):
+            a = pages.select_one('a')
+            if a is None:
+                a = None
+            else:
+                b = pages.select_one('a')['href']
+                print(b)
